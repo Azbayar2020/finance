f30b7180dc99631d1dcaf256aac429d829a95a32
Azbayar2020 <azaa.magbtc@gmail.com>
2020-04-29 18:10:16 +0800

    nemelt code

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 var uiController = (function() {
-
+    var DomStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        addBtn: '.add__btn',
+    };
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DomStrings.inputType).value,
+                description: document.querySelector(DomStrings.inputDescription).value,
+                value: document.querySelector(DomStrings.inputValue).value,
+            };
+        },
+        getDomStrings: function() {
+            return DomStrings;
+        }
+    };
 })();
 
 var financeController = (function() {
@@ -7,15 +24,30 @@ var financeController = (function() {
 })();
 
 var appController = (function(uiController, financeController) {
+
     var ctrlAddItem = function() {
-        alert("togshloo");
+        console.log(uiController.getInput());
     };
-    document.querySelector('.add__btn').addEventListener('click', function() {
-        ctrlAddItem();
-    });
-    document.addEventListener('keypress', function(event) {
-        if (event.keyCode === 13 || event.which === 13) {
+
+
+    var setupEventListeners = function() {
+        var DOM = uiController.getDomStrings();
+
+        document.querySelector(DOM.addBtn).addEventListener('click', function() {
             ctrlAddItem();
+        });
+        document.addEventListener('keypress', function(event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
+    return {
+        init: function() {
+            console.log('App starting');
+            setupEventListeners();
         }
-    });
+    }
 })(uiController, financeController);
+
+appController.init();
